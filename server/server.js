b72bb2a84bbe81016555c37853bb4d53db3c615e
VishalRajole <vishal.rajole@mobikontech.com>
2018-11-19 20:14:40 +0530

    added socket

--- server/server.js
+++ server/server.js
@@ -1,14 +1,24 @@
 const path = require("path");
 const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
 
-const app = express();
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, "../client");
 
+const app = express();
+var server = http.createServer(app);
+var socket = socketIO(server);
+
+socket.on("connection", socket => {
+  console.log("new user connected: ");
+  socket.on("disconnect", () => {
+    console.log("disconnected socket");
+  });
+});
+
 app.use(express.static(publicPath));
 
-app.get("/login", (req, res) => {});
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`server started on ${port}`);
 });
